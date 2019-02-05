40c5e675a14ba196e35f9fc014123aed89f30adf
Sravani Vedam <sravanivedam@zipcoders-MacBook-Pro.local>
2019-02-05 13:50:56 -0500

    Completed AliceAndBob assignment Sravani

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        String firstName;
+        String secondName;
+        System.out.println("Enter the first name : ");
+        firstName= scanner.nextLine();
+        System.out.println("Enter the second name : ");
+        secondName = scanner.nextLine();
+        
+        if ((firstName.equals("Alice")) || (secondName.equals("Bob")))
+        {
+        System.out.println("Greetings " + firstName + " " + secondName);
+       
+        }
 
     }
 }
